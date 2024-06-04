77a5fedbd28cbd8db163ae598fec41c0bfbbf5f2
mikeDev <miguelozanocobos@gmail.com>
2024-06-04 12:31:17 +0200

    exercise 1.6 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,18 +6,49 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const setOption = (value, option) =>{
+    if(option === "good"){
+      console.log("good increments")
+      console.log(value)
+      setGood(value+1)
+      return
+    } else if(option === "neutral"){
+      console.log("neutral increments")
+      console.log(value)
+      setNeutral(value+1)
+      return
+    } else if(option === "bad"){
+      console.log("bad increments")
+      console.log(value)
+      setBad(value+1)
+      return
+    }
+  }
+  
   return (
     <div>
       <h1>give feedback</h1>
-      //Button1
-      //Button2
-      //Button3
+      <Button handleClick={()=>setOption(good,"good")} option="good"/>
+      <Button handleClick={()=>setOption(neutral,"neutral")} option="neutral"/>
+      <Button handleClick={()=>setOption(bad,"bad")} option="bad"/>
       <h1>stadistics</h1>
-      <p>stadistics1</p>
-      <p>stadistics2</p>
-      <p>stadistics3</p>
+      <Display option="good" value={good} />
+      <Display option="neutral" value={neutral} />
+      <Display option="bad" value={bad} />
+
     </div>
   )
 }
+
+const Display = (props) =>(
+
+  <p>{props.option} {props.value}</p>
+)
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.option}
+  </button>
+)
 
 export default App
