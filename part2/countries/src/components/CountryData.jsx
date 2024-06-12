074c7cbdb6e4bca2875bc0d02e167316f3c9d3e2
mikeDev <miguelozanocobos@gmail.com>
2024-06-12 17:52:03 +0200

    exercise 2.20 countries

--- part2/countries/src/components/CountryData.jsx
+++ part2/countries/src/components/CountryData.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
 import Language from './Language'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 
 const CountryData = ({country}) =>{
         const languagesArray = Object.values(country.languages)
         const flags = Object.values(country.flags)
+        const api_key = import.meta.env.VITE_SOME_KEY
+        const apiURL = `https://my.meteoblue.com/packages/basic-1h_basic-day?apikey=${api_key}&lat=${country.latlng[0]}&lon=${country.latlng[0]}&format=json`
         
+        const [weather, setWeather] = useState([])
+        
+        useEffect(() => {
+          axios.get(apiURL)
+          .then(response => {
+            const apiResponse = response.data;
+            console.log("RESPUESTA API:",apiResponse)
+            setWeather([apiResponse])
+          }).catch(error => {
+            console.log(error);
+          })
+        }, [])
+        
+        if(weather.length>0){
+          return(
+            <div>
+              <h1>{country.name.official}</h1>
+              <p>Capital: ${country.capital[0]}</p>    
+              <p>Area: ${country.area}</p>
+              <h2>languages:</h2>
+                <ul>
+                  {languagesArray.map((lang,i) =>{
+                    
+                    return(<Language key={i} language={lang}/>)
+                  })}
+                </ul>
+              <img src={flags[0]}/>
+              <h2>Weather in {country.name.official}</h2>
+              <p>Temperature: {weather[0]["data_day"]["temperature_instant"][0]}</p>
+              <p>Wind: {weather[0]["data_day"]["windspeed_mean"][0]}</p>
+            </div>)
+        }
         return(
         <div>
           <h1>{country.name.official}</h1>
@@ -20,6 +56,7 @@ const CountryData = ({country}) =>{
               })}
             </ul>
           <img src={flags[0]}/>
+          
         </div>)
   
 }
