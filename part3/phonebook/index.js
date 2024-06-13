a846a4e97c81736834c28193ab08844b97398980
mikeDev <miguelozanocobos@gmail.com>
2024-06-13 11:36:45 +0200

    exercises 3.2 y 3.3 phonebook

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -31,6 +31,24 @@ app.get('/', (request, response) => {
   app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person=>person.id === id)
+    if(!person){
+        response.status(404).end()
+    } else{
+        response.json(person)
+    }
+    
+  })
+
+  app.get('/api/info', (request, response) => {
+    const entries = persons.length
+    response.send(`<p>Phonebook has info for ${entries} people</p>
+                   <p>${new Date()}<p/>`
+    )
+  })
   
   const PORT = 3001
   app.listen(PORT, () => {
