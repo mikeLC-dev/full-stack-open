4c71d20ad79738b63f3c6667f8104496725e18b6
mikeDev <miguelozanocobos@gmail.com>
2024-06-27 17:55:00 +0200

    exercises 4.6 and 4.7 bloglist

--- part4/bloglist/tests/helper.test.js
+++ part4/bloglist/tests/helper.test.js
@@ -89,3 +89,48 @@ describe('favorite blog',()=>{
 
 
 })
+
+
+describe('mostBlog',()=>{
+    test('of empty list is {}',()=>{
+        const resultEmptyListOfBlogs = listHelper.mostBlogs([])
+        assert.deepStrictEqual(resultEmptyListOfBlogs,{})
+    })
+
+    test('when list has only one blog equals that blog',()=>{
+        const resultOfOneBlog = listHelper.mostBlogs([blogs[1]])
+        assert.deepStrictEqual(resultOfOneBlog,{
+            author:"Edsger W. Dijkstra",
+            blogs:1
+        })
+    })
+
+    test('of a bigger list is calculated right',()=>{
+        const resultOfBiggerListBlogs = listHelper.mostBlogs(blogs)
+        assert.deepStrictEqual(resultOfBiggerListBlogs,{author:"Robert C. Martin", blogs: 3})
+    })
+
+
+})
+
+describe('mostLikes',()=>{
+    test('of empty list is {}',()=>{
+        const resultEmptyListOfBlogs = listHelper.mostLikes([])
+        assert.deepStrictEqual(resultEmptyListOfBlogs,{})
+    })
+
+    test('when list has only one blog equals that blog',()=>{
+        const resultOfOneBlog = listHelper.mostLikes([blogs[1]])
+        assert.deepStrictEqual(resultOfOneBlog,{
+            author:"Edsger W. Dijkstra",
+            likes: 5
+        })
+    })
+
+    test('of a bigger list is calculated right',()=>{
+        const resultOfBiggerListBlogs = listHelper.mostLikes(blogs)
+        assert.deepStrictEqual(resultOfBiggerListBlogs,{author:"Edsger W. Dijkstra", likes: 17})
+    })
+
+
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,25 +2,59 @@ const dummy = (blogs) => {
     return 1
   }
 
-  const totalLikes  = (blogs) => {
+const totalLikes  = (blogs) => {
     
     return blogs.length === 0 ? 0 : blogs.reduce((sum,blog)=> sum + blog.likes,0) 
     
   }
 
-  const favoriteBlog = (blogs) => {
+const favoriteBlog = (blogs) => {
     return blogs.length === 0 ? {} : blogs.reduce((maxLikes, blog) => blog.likes > maxLikes.likes ? blog : maxLikes, blogs[0])
-    /*
-    let blog = {}
-    if(blogs.length === 1){
-        blog = blogs[0]
-    } else if (blogs.length >= 1){
-        blog = blogs.reduce((maxLikes, blog) => blog.likes > maxLikes.likes ? blog : maxLikes, blogs[0]) 
-    }
-    return blog
-    */
 }
-  
+
+const mostBlogs = (blogs) =>{
+
+    let aux = []
+    if(blogs.length === 0){
+        return {}
+    } else {
+
+        blogs.forEach((blog) => {
+            if(!aux.some(e=>e.author === blog.author)){
+                aux.push({author: blog.author, blogs:1})
+            } else{                
+                aux.filter(e=>e.author === blog.author)[0].blogs += 1
+            }
+        })
+       
+        aux.sort((a,b)=>a.blogs - b.blogs)
+        return aux[aux.length-1]
+    }
+
+
+}
+
+const mostLikes = (blogs) =>{
+
+    let aux = []
+    if(blogs.length === 0){
+        return {}
+    } else {
+
+        blogs.forEach((blog) => {
+            if(!aux.some(e=>e.author === blog.author)){
+                aux.push({author: blog.author, likes:blog.likes})
+            } else{                
+                aux.filter(e=>e.author === blog.author)[0].likes += blog.likes
+            }
+        })
+       
+        aux.sort((a,b)=>a.likes - b.likes)
+        return aux[aux.length-1]
+    }
+
+}
+
   module.exports = {
-    dummy, totalLikes, favoriteBlog
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
   }
