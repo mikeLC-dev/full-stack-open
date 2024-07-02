eed0fb321a59a37a420519238237504ec9bd4f48
mikeDev <miguelozanocobos@gmail.com>
2024-07-02 07:53:20 +0200

    exercise 4.10 bloglist

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -20,6 +20,22 @@ test.only('there are one blogs', async () => {
     assert.strictEqual(response.body.length, 1)
   })
 
+test.only('new blog is created appropriately', async () => {
+
+    const blogPrueba = {
+        id:"654n6s5d4fh6a5sdf4j65dswaf4h",
+        title:"Blog de Prueba 1",
+        author:"Mike",
+        url:"https://pruebadeblog.com/",
+        likes:500
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogPrueba)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  })
 
 after(async () => {
   await mongoose.connection.close()
