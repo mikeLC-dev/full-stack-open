e5c4579474ad1253746f2ab45022c47c875c7d4c
mikeDev <miguelozanocobos@gmail.com>
2024-07-24 12:30:46 +0200

    exercise 5.18 modifications

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -36,6 +36,11 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.errorHandler)
 app.use(middleware.unknownEndpoint)
 module.exports = app;
