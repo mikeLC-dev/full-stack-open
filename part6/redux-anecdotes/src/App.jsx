9ed700df7ae5d163ffd1591efb3351c317193474
mikeLC-dev <miguelozanocobos@gmail.com>
2024-07-31 18:50:38 +0200

    exercise 6.5 and 6.6 redux-anexdotes

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -4,6 +4,7 @@ import reducer from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
+  anecdotes.sort((a, b) => b.votes - a.votes)
   const dispatch = useDispatch()
 
   const vote = (id) => {
